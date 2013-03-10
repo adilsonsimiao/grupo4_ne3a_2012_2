8634a08254c1fb9fce57ebb70d7d372742c29bf7
adilsonsimiao <adilsonsimiao@hotmail.com>
2013-03-10 07:32:15 -0300

    remoção de uns pacotes não utilizados

--- GerenciadorOS/src/br/com/ordemDeServico/model/dao/ClienteDao.java
+++ GerenciadorOS/src/br/com/ordemDeServico/model/dao/ClienteDao.java
@@ -10,8 +10,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -69,7 +67,7 @@ public class ClienteDao  {
         }
     }
 
-    public void update(Cliente cliente)throws DaoException { 
+    public void update(Cliente cliente) { 
         Connection con = ConnectionProvider.getConection();
 
         try {
