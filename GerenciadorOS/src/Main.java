8596ac54dac0b24422d2defc24fbfb5bf105ce24
adilsonsimiao <adilsonsimiao@hotmail.com>
2013-04-19 12:43:58 -0300

    #30 Classe Main com tela

--- GerenciadorOS/src/Main.java
+++ GerenciadorOS/src/Main.java
@@ -1,5 +1,6 @@
 
 import br.com.ordemDeServico.view.CadastrarCliente;
+import br.com.ordemDeServico.view.ConsultarCliente;
 
 /*
  * To change this template, choose Tools | Templates
@@ -38,13 +39,9 @@ public class Main extends javax.swing.JFrame {
         jMenuItem4 = new javax.swing.JMenuItem();
         jMenuItem5 = new javax.swing.JMenuItem();
         jMCons = new javax.swing.JMenu();
-        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenConsultar = new javax.swing.JMenuItem();
         jMenuItem6 = new javax.swing.JMenuItem();
         jMenuItem7 = new javax.swing.JMenuItem();
-        jMenu6 = new javax.swing.JMenu();
-        jMenuItem8 = new javax.swing.JMenuItem();
-        jMenuItem9 = new javax.swing.JMenuItem();
-        jMenuItem10 = new javax.swing.JMenuItem();
 
         jMenuItem1.setText("jMenuItem1");
 
@@ -91,8 +88,13 @@ public class Main extends javax.swing.JFrame {
 
         jMCons.setText("CONSULTAR");
 
-        jMenuItem2.setText("CLIENTE");
-        jMCons.add(jMenuItem2);
+        jMenConsultar.setText("CLIENTE");
+        jMenConsultar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenConsultarActionPerformed(evt);
+            }
+        });
+        jMCons.add(jMenConsultar);
 
         jMenuItem6.setText("AUTOMOVEL");
         jMCons.add(jMenuItem6);
@@ -101,19 +103,6 @@ public class Main extends javax.swing.JFrame {
         jMCons.add(jMenuItem7);
 
         jMenuBar1.add(jMCons);
-
-        jMenu6.setText("REMOVER");
-
-        jMenuItem8.setText("CLIENTE");
-        jMenu6.add(jMenuItem8);
-
-        jMenuItem9.setText("AUTMOVEL");
-        jMenu6.add(jMenuItem9);
-
-        jMenuItem10.setText("SERVIÇO");
-        jMenu6.add(jMenuItem10);
-
-        jMenuBar1.add(jMenu6);
 
         setJMenuBar(jMenuBar1);
 
@@ -140,56 +129,24 @@ public class Main extends javax.swing.JFrame {
         cadCli.setVisible(true);
     }//GEN-LAST:event_jMenuItem3ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+    private void jMenConsultarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenConsultarActionPerformed
+        ConsultarCliente consCli = new ConsultarCliente();
+        consCli.setVisible(true);
+    }//GEN-LAST:event_jMenConsultarActionPerformed
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Main().setVisible(true);
-            }
-        });
-    }
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
     private javax.swing.JMenu jMCad;
     private javax.swing.JMenu jMCons;
-    private javax.swing.JMenu jMenu6;
+    private javax.swing.JMenuItem jMenConsultar;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem10;
-    private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JMenuItem jMenuItem4;
     private javax.swing.JMenuItem jMenuItem5;
     private javax.swing.JMenuItem jMenuItem6;
     private javax.swing.JMenuItem jMenuItem7;
-    private javax.swing.JMenuItem jMenuItem8;
-    private javax.swing.JMenuItem jMenuItem9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
--- GerenciadorOS/src/Teste/MainTest.java
+++ GerenciadorOS/src/Teste/MainTest.java
@@ -4,10 +4,12 @@
  */
 package Teste;
 
-import br.com.ordemDeServico.model.dao.Daos;
+import br.com.ordemDeServico.model.dao.ClienteDao;
 import br.com.ordemDeServico.model.entity.Cliente;
 import br.com.ordemDeServico.model.entity.Endereco;
 import br.com.ordemDeServico.model.entity.Telefone;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -16,38 +18,56 @@ import br.com.ordemDeServico.model.entity.Telefone;
 public class MainTest {
 
     public static void main(String[] args) throws Exception {
-
-        Daos daoCli = new Daos();
-        Cliente cliente = new Cliente();
-        cliente.setNome("adilson");
-        cliente.setCpf("8800000");
-        cliente.setRg("999999");
-
-        Endereco end = new Endereco();
-        end.setBairro("reqs");
-        end.setCep("00909");
-        end.setCidade("maringa");
-        end.setComplemento("casa");
-        end.setLogradouro("said");
-        end.setUf("PR");
-
-        Telefone tel = new Telefone();
-        tel.setDdd("44");
-        tel.setTelefone("4444");
-        Telefone tel1 = new Telefone();
-        tel.setDdd("44");
-        tel.setTelefone("8888");
-
-        cliente.setTelefone(tel);
-        cliente.setTelefone(tel1);
-        cliente.setEndereco(end);
+          ClienteDao clienteDao = new ClienteDao();
+          
+   List<Cliente> clientes = new ArrayList<>();
+//          
+       
+       Cliente cliente = new Cliente();
+//        cliente.setNome("adilson");
+//        cliente.setCpf("8800000");
+//        cliente.setRg("999999");
+//
+//        Endereco end = new Endereco();
+//        end.setBairro("reqs");
+//        end.setCep("00909");
+//        end.setCidade("maringa");
+//        end.setComplemento("casa");
+//        end.setLogradouro("said");
+//        end.setUf("PR");
+//
+//        Telefone tel = new Telefone();
+//        tel.setDdd("44");
+//        tel.setTelefone("4444");
+//        Telefone tel1 = new Telefone();
+//        tel.setDdd("44");
+//        tel.setTelefone("8888");
+//
+//        cliente.setTelefone(tel);
+//        cliente.setTelefone(tel1);
+//        cliente.setEndereco(end);
         
-        daoCli.persiste(cliente);
-        daoCli.fechaSessão();
+//        daoCli.persiste(cliente);
+//        
+       
+    cliente= clienteDao.retrieveId(30);
+    //clientes =clienteDao.alterarPorNome("adilson");
+   
+    
+    
+    
+    
+        System.out.println(cliente.toString());
+      
+     
+       
+       
+//      
+     
 
-
-
-
+ 
+      
+    
 
 
     }
