7eec4cc24da552e14cfbe2614d80c8046cd17ab8
adilsonsimiao <adilsonsimiao@hotmail.com>
2013-04-24 17:01:05 -0300

    #29 hibernate atualizado

--- GerenciadorOS/src/br/com/ofiSys/model/dao/HibernateUtil.java
+++ GerenciadorOS/src/br/com/ofiSys/model/dao/HibernateUtil.java
@@ -19,9 +19,7 @@ public class HibernateUtil {
     
     static {
         try {
-            // Create the SessionFactory from standard (hibernate.cfg.xml) 
-            // config file.            
-            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+                       sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
         } catch (Throwable ex) {
             // Log the exception. 
             System.err.println("Initial SessionFactory creation failed." + ex);
